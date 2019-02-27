5f84207bbdc391b514fa5feaf4898506ed85e2b0
abbie <abigailzn@yahoo.com>
2019-02-26 17:44:07 -0700

    fixing synth naming bug

--- script.js
+++ script.js
@@ -11,10 +11,6 @@ var listening = false;
 var reverb = new Tone.JCReverb(0.4).connect(Tone.Master);
 var delay = new Tone.FeedbackDelay(0.5);
 var synth =new Tone.DuoSynth().chain(delay, reverb);
-var distortion = new Tone.Distortion(0.6)
-var tremolo = new Tone.Tremolo().start()
-
-var polySynth = new Tone.PolySynth(4, Tone.Synth).chain(distortion, tremolo, Tone.Master)
 
 document.getElementById("connect").addEventListener("click", function() {
     console.log("Button clicked");
@@ -40,80 +36,80 @@ var orientationHandler = function(event) {
     gamma = event.gamma
     document.getElementById("status").innerHTML = "Alpha " + event.alpha + " Beta " + event.beta + " Gamma " + event.gamma;
     if (beta <= -160) {
-        polySynth.volume.value = -10
+        synth.volume.value = -10
     } else if (beta <= -140 && beta > -160) {
-        polySynth.volume.value = -12;
+        synth.volume.value = -12;
     } else if (beta <= -120 && beta > -140) {
-        polySynth.volume.value = -14;
+        synth.volume.value = -14;
     } else if (beta <= -100 && beta > -120) {
-        polySynth.volume.value = -16;
+        synth.volume.value = -16;
     } else if (beta <= -80 && beta > -100) {
-        polySynth.volume.value = -18;
+        synth.volume.value = -18;
     } else if (beta <= -60 && beta > -80) {
-        polySynth.volume.value = -20;
+        synth.volume.value = -20;
     } else if (beta <= -40 && beta > -60) {
-        polySynth.volume.value = -22;
+        synth.volume.value = -22;
     } else if (beta <= -20 && beta > -40) {
-        polySynth.volume.value = -24;
+        synth.volume.value = -24;
     } else if (beta <= 0 && beta > -20) {
-        polySynth.volume.value = -26;
+        synth.volume.value = -26;
     } else if (beta <= 20 && beta > 0) {
-        polySynth.volume.value = -28;
+        synth.volume.value = -28;
     } else if (beta <= 40 && beta > 20) {
-        polySynth.volume.value = -30;
+        synth.volume.value = -30;
     } else if (beta <= 60 && beta > 40) {
-        polySynth.volume.value = -28;
+        synth.volume.value = -28;
     } else if (beta <= 80 && beta > 60) {
-        polySynth.volume.value = -30;
+        synth.volume.value = -30;
     } else if (beta <= 100 && beta > 80) {
-        polySynth.volume.value = -32;
+        synth.volume.value = -32;
     } else if (beta <= 120 && beta > 100) {
-        polySynth.volume.value = -34;
+        synth.volume.value = -34;
     } else if (beta <= 140 && beta >120) {
-        polySynth.volume.value = -36;
+        synth.volume.value = -36;
     } else if (beta <= 160 && beta > 140) {
-        polySynth.volume.value = -38;
+        synth.volume.value = -38;
     } else if (beta > 160) {
-        polySynth.volume.value = -40;
+        synth.volume.value = -40;
     } else {
-        polySynth.volume.value = -42;
+        synth.volume.value = -42;
     }
 
     console.log("synth.volume.value: " + synth.volume.value);
 
     if (gamma <= -70) {
-        polySynth.triggerAttackRelease("C4", "32n");
+        synth.triggerAttackRelease("C4", "64n");
     } else if (gamma <= -60 && gamma > -70) {
-        polySynth.triggerAttackRelease("D4", "32n");
+        synth.triggerAttackRelease("D4", "64n");
     } else if (gamma <= -50 && gamma > -60) {
-        polySynth.triggerAttackRelease("E4", "32n");
+        synth.triggerAttackRelease("E4", "64n");
     } else if (gamma <= -40 && gamma > -50) {
-        polySynth.triggerAttackRelease("F4", "32n");
+        synth.triggerAttackRelease("F4", "64n");
     } else if (gamma <= -30 && gamma > -40) {
-        polySynth.triggerAttackRelease("G4", "32n");
+        synth.triggerAttackRelease("G4", "64n");
     } else if (gamma <= -20 && gamma > -30) {
-        polySynth.triggerAttackRelease("A5", "32n");
+        synth.triggerAttackRelease("A5", "64n");
     } else if (gamma <= -10 && gamma > -20) {
-        polySynth.triggerAttackRelease("C5", "32n");
+        synth.triggerAttackRelease("C5", "64n");
     } else if (gamma <= 0 && gamma > -10) {
-        polySynth.triggerAttackRelease("D5", "32n");
+        synth.triggerAttackRelease("D5", "64n");
     } else if (gamma <= 10 && gamma > 0) {
-        polySynth.triggerAttackRelease("E5", "32n");
+        synth.triggerAttackRelease("E5", "64n");
     } else if (gamma <= 20 && gamma > 10) {
-        polySynth.triggerAttackRelease("F5", "32n");
+        synth.triggerAttackRelease("F5", "64n");
     } else if (gamma <= 30 && gamma > 20) {
-        polySynth.triggerAttackRelease("G5", "32n");
+        synth.triggerAttackRelease("G5", "64n");
     } else if (gamma <= 40 && gamma > 30) {
-        polySynth.triggerAttackRelease("A5", "32n");
+        synth.triggerAttackRelease("A5", "64n");
     } else if (gamma <= 50 && gamma > 40) {
-        polySynth.triggerAttackRelease("C6", "32n");
+        synth.triggerAttackRelease("C6", "64n");
     } else if (gamma <= 60 && gamma > 50) {
-        polySynth.triggerAttackRelease("D6", "32n");
+        synth.triggerAttackRelease("D6", "64n");
     } else if (gamma <= 70 && gamma > 60) {
-        polySynth.triggerAttackRelease("E6", "32n");
+        synth.triggerAttackRelease("E6", "64n");
     } else if (gamma > 70) {
-        polySynth.triggerAttackRelease("F6", "32n");
+        synth.triggerAttackRelease("F6", "64n");
     } else {
-        polySynth.triggerAttackRelease("G6", "32n");
+        synth.triggerAttackRelease("G6", "64n");
     }
 };
